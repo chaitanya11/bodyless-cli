1193f91786501010d73ffe67c52ccb68e3d16376
Chaitanya Garikipati <chaitanya.garikipati123@gmail.com>
2018-08-04 19:34:20 +0530

    adding subcommands

--- main.go
+++ main.go
@@ -15,13 +15,47 @@ var (
 	codeBucket    string
 	contentBucket string
 	profile       string
+	createCommand *flag.FlagSet
+	buildCommand  *flag.FlagSet
+	deployCommand *flag.FlagSet
 )
 
 func main() {
-	flag.Parse()
-	// if user does not supply flags, print usage
-	if flag.NFlag() == 0 {
-		printUsage()
+
+	commands := []*flag.FlagSet{
+		createCommand,
+		buildCommand,
+		deployCommand,
+	}
+
+	if len(os.Args) == 1 {
+		printHelp(commands)
+		return
+	}
+
+	switch os.Args[1] {
+	case "create":
+		createCommand.Parse(os.Args[2:])
+	case "build":
+		buildCommand.Parse(os.Args[2:])
+	case "deploy":
+		deployCommand.Parse(os.Args[2:])
+	default:
+		printHelp(commands)
+		os.Exit(2)
+	}
+	// flag.Parse()
+	// // if user does not supply flags, print usage
+	// if flag.NFlag() == 0 {
+	// 	printUsage()
+	// }
+
+	for _, command := range commands {
+		if command.Parsed() {
+			if command.NFlag() == 0 {
+				printDefaults(command)
+			}
+		}
 	}
 
 	// clone cms repo in to path
@@ -36,16 +70,41 @@ func main() {
 }
 
 func init() {
-	flag.StringVarP(&projectName, "ProjectName", "N", "", "Name of the project.")
-	flag.StringVarP(&path, "Path", "P", ".", "Project Location.")
-	flag.StringVarP(&codeBucket, "CodeBucketName", "w", "", "Name of the bucket where website code is deployed.")
-	flag.StringVarP(&contentBucket, "ContentBucketName", "c", "", "Name of the bucket where content is stored.")
-	flag.StringVarP(&profile, "profile", "p", "default", "Name of the aws profile configured.")
+	// create command
+	createCommand = flag.NewFlagSet("create", flag.ExitOnError)
+	createCommand.StringVarP(&projectName, "ProjectName", "N", "", "Name of the project.")
+	createCommand.StringVarP(&path, "Path", "P", ".", "Project Location.")
+	createCommand.StringVarP(&codeBucket, "CodeBucketName", "w", "", "Name of the bucket where website code is deployed.")
+	createCommand.StringVarP(&contentBucket, "ContentBucketName", "c", "", "Name of the bucket where content is stored.")
+	createCommand.StringVarP(&profile, "profile", "p", "default", "Name of the aws profile configured.")
+
+	buildCommand = flag.NewFlagSet("build", flag.ExitOnError)
+	deployCommand = flag.NewFlagSet("deploy", flag.ExitOnError)
 }
 
 func printUsage() {
-	fmt.Printf("Usage: %s [options]\n", os.Args[0])
-	fmt.Println("Options:")
-	flag.PrintDefaults()
+	// fmt.Printf("Usage: %s [options]\n", os.Args[0])
+	// fmt.Println("Options:")
+	// flag.PrintDefaults()
+
+	if len(os.Args) == 1 {
+		fmt.Println("usage: bodyless <command> [<args>]")
+		return
+	}
 	os.Exit(1)
+}
+
+func printDefaults(command *flag.FlagSet) {
+	command.PrintDefaults()
+}
+
+func printHelp(commands []*flag.FlagSet) {
+	fmt.Println("usage: bodyless <command> [<args>]")
+	fmt.Println("commands:")
+	fmt.Println("create")
+	createCommand.PrintDefaults()
+	fmt.Println("build")
+	buildCommand.PrintDefaults()
+	fmt.Println("deploy")
+	deployCommand.PrintDefaults()
 }
